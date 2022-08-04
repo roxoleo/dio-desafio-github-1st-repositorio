c606b6065186cbaa01dc5f88e9521ea9d3ca9f4c
Léo Roxo <52751417+roxoleo@users.noreply.github.com>
2022-08-04 11:32:50 -0300

    Add files via upload
    upload conteúdo Introdução ao JavaScript com Stephany Nusch - DIO

---
+++ M2.4-Introdução_JavaScript/aula2-Recursos_Básicos/aula2.1-manipulando_arquivo_js/teste.js
@@ -0,0 +1,27 @@
+// comentário de linha          atalho: Ctrl+;
+/* comentário várias linhas     atalho: Shift+Alt+A */
+
+/* 
+// var y = 0;
+var x = 1;  // var = variável = símbolo que representa valor desconhecido ou a ser atribuído
+
+var y = 0;
+var x = 1;
+
+var z = 2;
+*/
+
+// variáveis em equação matemática
+// ax + b = 0;
+
+//f(1) = a*1 + b
+
+// declarando variáveis e constantes
+/* em JS é importante que
+1- as variáveis e constantes tenham sentido, signifiquem algo
+2- as variáveis sejam escritas em minúsculo e as constantes em maíusculo
+
+var preco = 2;
+var desconto = 0.2;
+preco = preco - desconto;
+*/
---
+++ M2.4-Introdução_JavaScript/aula2-Recursos_Básicos/aula2.2-funcoes/funcao.js
@@ -0,0 +1,6 @@
+function soma(a, b){
+    //console.log(a + b);
+    return a + b;
+}
+
+soma(3, 5);
